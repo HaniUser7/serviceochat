68a4c543326235f0873dea6056f1ff296378e56a
HaniUser7 <hani7sharma@gmail.com>
2022-06-01 13:14:54 +0530

    add new file..

---
+++ connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGOURL)
+    .then(() => console.log('Database connected successfully.'))
+    .catch(err => console.log(err));
+
+
+
+module.exports = { mongoose };
